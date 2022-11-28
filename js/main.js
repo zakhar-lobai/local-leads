c81dc121a8032d3c87ef650fd34e0de6783f5f88
Zakhar_Lobai <lobajzahar@gmail.com>
2022-11-28 03:03:47 +0200

    Home page mob-friendly without burger menu

--- js/main.js
+++ js/main.js
@@ -14,3 +14,16 @@ $('.slider__btn--prev').click(function () {
 $('.slider__btn--next').click(function () {
 	owl.trigger('next.owl.carousel');
 });
+
+
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
+const menuIcon = document.querySelector('.menu-icon');
+
+
+navBtn.onclick = function () {
+	nav.classList.toggle('nav--mobile')
+	menuIcon.classList.toggle('menu-icon--active');
+	document.body.classList.toggle('no-scroll');
+};
